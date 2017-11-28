f88469dc2b2ea4afff71bacab75fffe3d2577097
dastangoo <hamed.dastangoo@gmail.com>
2017-11-28 18:29:09 +0330

    Introduction to Section 2 and Ruby

---
+++ ruby_projects/helloworldmethod.rb
@@ -0,0 +1,7 @@
+def hello(anything)
+  puts anything
+end
+
+hello "My name is Johne Doe."
+hello "Welcome to the Complete Ruby on Rails Developer Course"
+hello "Check it out this prints anyting I want"
